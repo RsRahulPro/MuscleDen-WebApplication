6e2618af2412f584cdc04070c41673be8a90d4e8
Rahul Sharma <rs.rahulpro@gmail.com>
2022-06-14 00:28:19 +0530

    Subject: Create Navigation bar and style it 1. Create and style the navigation bar component using the stack    component from material UI 2. Change Font of the entire website to montserrat from google fonts

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import {Box} from "@mui/material";
 
 import "./App.css";
 
-const App = () => {
-  return (
-    <>
-      <Box width="400px">
-        Navbar
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/exercise/:id" element={<ExerciseDetail />} />
-        </Routes>
-      </Box>
-    </>
-  );
-};
+//importing pages
+import ExerciseDetail from "./Pages/ExerciseDetail";
+import Home from "./Pages/Home";
+
+//importing components
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+const App = () => (
+  <Box width="400px" sx={{width: {xl: "1488px"}}} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
 
 export default App;
---
+++ src/Pages/ExerciseDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExerciseDetail = () => {
+  return <div>This is the Exercise detail page</div>;
+};
+
+export default ExerciseDetail;
